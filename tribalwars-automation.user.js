10b08c258abf95fb1d3bd8eb92399278a767b934
C7696 <128704167+C7696@users.noreply.github.com>
2025-01-05 08:35:55 -0300

    Update tribalwars-automation.user.js

--- tribalwars-automation.user.js
+++ tribalwars-automation.user.js
@@ -1,11 +1,1 @@
-// ==UserScript==
-// @name        Tribal Wars Automation
-// @namespace   http://tampermonkey.net/
-// @version     3.0
-// @description Script para Tribal Wars com atualizações automáticas do GitHub.
-// @author      Você
-// @match       https://*.tribalwars.com.br/*
-// @grant       none
-// @updateURL   https://raw.githubusercontent.com/seu-usuario/TribalWars-Automation/main/tribalwars-automation.user.js
-// @downloadURL https://raw.githubusercontent.com/seu-usuario/TribalWars-Automation/main/tribalwars-automation.user.js
-// ==/UserScript==
+
